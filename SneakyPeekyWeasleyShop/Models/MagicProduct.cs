80947b0e2638d48bbf41c37cd7ed9a8eef054155
Maks0s <mrmaksman01@mail.ru>
2023-04-06 12:02:40 +0300

    Created MagicProduct in Models

---
+++ SneakyPeekyWeasleyShop/Models/MagicProduct.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace SneakyPeekyWeasleyShop.Models
+{
+    public class MagicProduct
+    {
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+
+        [JsonPropertyName("img")]
+        public string? Image { get; set; }
+
+        [JsonPropertyName("url")]
+        public string? URL { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<MagicProduct>(this);
+    }
+}
