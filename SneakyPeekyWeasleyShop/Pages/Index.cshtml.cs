7958c4b03e05cef00727f4add9fda22352e1627e
Maks0s <mrmaksman01@mail.ru>
2023-04-07 19:13:41 +0300

    Added grab logic for products from the DB

--- SneakyPeekyWeasleyShop/Pages/Index.cshtml.cs
+++ SneakyPeekyWeasleyShop/Pages/Index.cshtml.cs
@@ -1,20 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SneakyPeekyWeasleyShop.Models;
+using SneakyPeekyWeasleyShop.Services;
 
 namespace SneakyPeekyWeasleyShop.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly JsonFileMagicProductService _magicProductService;
+        public IEnumerable<MagicProduct> MagicProducts { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileMagicProductService magicProductService)
         {
             _logger = logger;
+            _magicProductService = magicProductService;
         }
 
         public void OnGet()
         {
-
+            MagicProducts = _magicProductService.GetMagicProducts();
         }
     }
 }
