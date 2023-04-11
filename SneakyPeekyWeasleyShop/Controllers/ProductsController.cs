36a538ac8aa4aec20ce9da089069d6248dba54f0
Maks0s <mrmaksman01@mail.ru>
2023-04-11 19:12:52 +0300

    added API for getting products

---
+++ SneakyPeekyWeasleyShop/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SneakyPeekyWeasleyShop.Models;
+using SneakyPeekyWeasleyShop.Services;
+
+namespace SneakyPeekyWeasleyShop.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileMagicProductService jsonFileMagicProductService)
+        {
+            _jsonFileMagicProductService = jsonFileMagicProductService;
+        }
+
+        private JsonFileMagicProductService _jsonFileMagicProductService;
+
+        [HttpGet]
+        public IEnumerable<MagicProduct> Get()
+        {
+            return _jsonFileMagicProductService.GetMagicProducts();
+        }
+
+    }
+}
